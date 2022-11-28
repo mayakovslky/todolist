05b53dc59f3ec7de7a9553f84f942916cb5fa2e3
mayakovsky <mayakovsky03@gmail.com>
2022-11-28 15:25:53 +0300

    add new project Todolist

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,23 @@ import './App.css';
 import {Todolist} from "./Todolist";
 
 function App() {
+
+    const task1 = [
+        {id: 1, title: 'HTML&CSS', isDone: true },
+        {id: 2, title: 'JS', isDone: true },
+        {id: 3, title: 'ReactJS', isDone: true },
+    ]
+
+    const task2 = [
+        {id: 1, title: 'Hello world', isDone: true },
+        {id: 2, title: 'I am happy', isDone: true },
+        {id: 3, title: 'Yo', isDone: true },
+    ]
+
     return (
         <div className="App">
-            <Todolist title={'What to learn'}/>
-            <Todolist title={'Books'}/>
-            <Todolist title={'Songs'}/>
+            <Todolist title={'What to learn'} tasks={task1}/>
+            <Todolist title={'Books'} tasks={task2}/>
         </div>
     );
 }
